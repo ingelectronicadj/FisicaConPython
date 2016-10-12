5cfdff7cbe456676c510af2bdd41d9d8f40d0efc
Diego Javier Mena Amado <glud@localhost.localdomain>
2016-10-12 18:15:17 -0500

    Se añade un checkBox para multiples botones

--- tutoBokeh/introBokeh.py
+++ tutoBokeh/introBokeh.py
@@ -1,13 +1,21 @@
-""" ********************************************************************
+""" ******************************************************
  *              LIBRERIAS y CONSTANTES
- ***********************************************************************
+ *********************************************************
 """
 from bokeh.io import output_file, show
 from bokeh.layouts import widgetbox
 from bokeh.models.widgets import Button
+from bokeh.models.widgets import CheckboxButtonGroup
+
 
 output_file("tutoGLUD.html")
-
+#Ejemplo de un boton
 button = Button(label="Ejemplo-Boton", button_type="success")
 
-show(widgetbox(button))
+#Ejemplo de un checkBox para multiples botones
+checkbox_button_group = CheckboxButtonGroup(
+        labels=["Boton 1", "Boton 2", "Boton 3"], active=[0, 1])
+
+show(widgetbox(button, checkbox_button_group))
+
+
