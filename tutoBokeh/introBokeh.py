6403fbb3b918dc2cfaa29f2f256108bea6876330
Diego Javier Mena Amado <glud@localhost.localdomain>
2016-10-12 18:23:14 -0500

    Se añadeejemplo del standard checkBox

--- tutoBokeh/introBokeh.py
+++ tutoBokeh/introBokeh.py
@@ -4,18 +4,20 @@
 """
 from bokeh.io import output_file, show
 from bokeh.layouts import widgetbox
-from bokeh.models.widgets import Button
-from bokeh.models.widgets import CheckboxButtonGroup
-
+from bokeh.models.widgets import Button, CheckboxButtonGroup, CheckboxGroup
 
 output_file("tutoGLUD.html")
 #Ejemplo de un boton
-button = Button(label="Ejemplo-Boton", button_type="success")
+boton = Button(label="Ejemplo-Boton", button_type="success")
 
 #Ejemplo de un checkBox para multiples botones
-checkbox_button_group = CheckboxButtonGroup(
+multiBoton = CheckboxButtonGroup(
         labels=["Boton 1", "Boton 2", "Boton 3"], active=[0, 1])
 
-show(widgetbox(button, checkbox_button_group))
+#Ejemplo de un Standard para un checkBox
+checkboxBoton = CheckboxGroup(
+        labels=["Opcion A", "Opcion B", "Opcion C"], active=[0, 1])
+
+show(widgetbox(boton, multiBoton, checkboxBoton))
 
 
