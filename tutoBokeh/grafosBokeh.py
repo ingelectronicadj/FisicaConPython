fd9f531e93b30448c6834156c32c4ddcd19265eb
root <root@localhost.localdomain>
2016-10-13 12:20:24 -0500

    Se arreglan algunos parametros de la grafica del tutoBokeh (tamaño del workspace, se añaden titulos y dominio del plot)

--- tutoBokeh/grafosBokeh.py
+++ tutoBokeh/grafosBokeh.py
@@ -1,15 +1,18 @@
-from bokeh.models.widgets import Panel, Tabs
 from bokeh.io import output_file, show
+from bokeh.models.widgets import Panel, Tabs
 from bokeh.plotting import figure
 
 output_file("graficos.html")
 
-p1 = figure(plot_width=1200, plot_height=300)
-p1.circle([1, 2, 3, 4, 5, 6, 7, 8], [6, 7, 2, 4, 5, 3, 8, 4], size=20, color="navy", alpha=0.5)
+
+dominioTemporal=[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+p1 = figure(plot_width=1200, plot_height=300, title="Todo es facil")
+p1.circle(dominioTemporal, [5, 1, 6, 7, 2, 4, 5, 3, 8, 4, 2], size=20, color="navy", alpha=0.5)
 tab1 = Panel(child=p1, title="Grafico puntos")
 
-p2 = figure(plot_width=1200, plot_height=300)
-p2.line([1, 2, 3, 4, 5, 6, 7, 8], [6, 7, 2, 4, 5, 3, 8, 4], line_width=3, color="navy", alpha=0.5)
+p2 = figure(plot_width=1200, plot_height=300, title="Cuando ya esta hecho")
+p2.line(dominioTemporal, [5, 1, 6, 7, 2, 4, 5, 3, 8, 4, 2], line_width=3, color="navy", alpha=0.5)
 tab2 = Panel(child=p2, title="Grafico lineal")
 
 tabs = Tabs(tabs=[ tab1, tab2 ])
