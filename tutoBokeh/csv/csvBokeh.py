9bbcff0ee34290cb6774740091d86ab994a7ba7a
Diego Mena <ingelectronicadj@gmail.com>
2016-10-13 18:29:03 -0500

    Se añade ejemplo de grafico consumiendo archivo .csv

---
+++ tutoBokeh/csv/csvBokeh.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from bokeh.plotting import figure, output_file, show
+
+misDatos = pd.read_csv("/home/glud/Documentos/FisicaConPython/tutoBokeh/csv/table.csv", parse_dates=['Date'])
+
+output_file("tomaLoTuyo.html")
+
+# create a new plot with a datetime axis type
+p = figure(width=1100, height=300, x_axis_type="datetime", title='Graficando consumiendo fichero .csv')
+
+p.line(misDatos['Date'], misDatos['Volume'], color='navy', alpha=0.5) #Columnas: Date,Open,High,Low,Close,Volume,Adj Close
+
+show(p)
